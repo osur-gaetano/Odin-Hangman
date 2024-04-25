9eb4339d4c11c9b03c1446cade8e2d03c7a23433
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-04-26 01:13:33 +0300

    Refactor Player classes

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,6 @@
-# frozen_string_literal:false
+# frozen_string_literal: false
 
+# Player class
 class Player
   attr_accessor :name
 
@@ -8,23 +9,13 @@ class Player
   end
 end
 
-class ComputerPlayer < Player
-  attr_accessor :secret
-
-  def initialize(name = 'Javis')
-    super(name)
-  end
-
-  def secret_word(secret)
-    @secret = secret
-  end
-end
-
+# HumanPlayer Class
+# Inherits from Player class
 class HumanPlayer < Player
   attr_accessor :guess, :wins
 
   def initialize(name)
-    super(name)
+    super
     @wins = 0
   end
 
@@ -34,13 +25,15 @@ class HumanPlayer < Player
   end
 end
 
-class HangManGame
-  attr_accessor :secret_word, :player_one, :computer_player, :word_file
+# ComputerPlayer class
+# Inherits from the Player class
+class ComputerPlayer < Player
+  attr_accessor :secret, :word_file
 
-  def initialize(computer_player, human_player, file)
-    @computer_player = computer_player
-    @player_one = human_player
-    @word_file = file
+  def initialize(word_file, name = 'Javis')
+    super(name)
+    @secret = nil
+    @word_file = word_file
   end
 
   def generate_secret_word
@@ -48,47 +41,10 @@ class HangManGame
     File.foreach(@word_file) do |line|
       word_list.append(line.downcase.chomp) if line.chomp.length > 5 && line.chomp.length < 13
     end
-    @secret_word = word_list.sample
-  end
-
-  def play
-    @computer_player.secret_word(@secret_word)
-    guess_numbers_aloowed = (@secret_word.length * 1.5 + 1)
-    current_game_secret = @computer_player.secret.split('')
-    current_geme_guess = Array.new(current_game_secret.length, '_')
-
-    iteration = 0
-    loop do
-      if iteration < guess_numbers_aloowed
-        p "This is iteration #{iteration} \n"
-        puts "#{player_one.name} make your guess\n"
-        current_player_one_guess = player_one.make_guess
-        if current_game_secret.include? current_player_one_guess
-          current_game_secret.each_index.select do |index|
-            current_geme_guess[index] = current_player_one_guess if current_game_secret[index] == current_player_one_guess
-          end
-        end
-        puts "The current game_secret is : #{current_game_secret.join(' ')} \n"
-        puts "The current guess state is : #{current_geme_guess.join(' ')} \n"
-        break if current_geme_guess.eql? current_game_secret
-      else
-        puts "You've had your chances and its time to 'Hang THE Man'!!! \n"
-        break
-      end
-      iteration += 1
-    end
+    @secret = word_list.sample
   end
 end
 
 puts "-*-*-*-*-*-*-*-*-*-*- Let's Hang the Man -*-*-*-*-*-*-*-*-*-*-*-*-"
 
-word_file = 'google-10000-english-no-swears.txt'
-
-puts 'What is your name, human player? '
-human_name = gets.downcase.chomp
-human_player = HumanPlayer.new(human_name)
-computer_player = ComputerPlayer.new
-
-game = HangManGame.new(computer_player, human_player, word_file)
-game.generate_secret_word
-game.play
+wordlist_file = 'google-10000-english-no-swears.txt'
